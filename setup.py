675d2ec348e78aead468ebe7bd9beaf019127155
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-05-18 15:11:07 +0200

    changed app name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-        name="docker-image-save",
+        name="dockersave",
         version="0.1",
         author="Adam Olech",
         author_email="aolech@antmicro.com",
